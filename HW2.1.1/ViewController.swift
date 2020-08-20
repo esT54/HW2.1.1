0344d72632dd92b4f3d6667d9455006b40209511
est54 <smirnyagin.semen@gmail.com>
2020-08-20 16:46:12 +0700

    Извиняюсь за констрейнты, а собственно за их отсутствие. Не посчитай это как не уважение к тебе. Реально не успевал.

--- HW2.1.1/ViewController.swift
+++ HW2.1.1/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var pressButton: UIButton!
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redButton.layer.cornerRadius = 45
+        yellowButton.layer.cornerRadius = 45
+        greenButton.layer.cornerRadius = 45
     }
 
-
+    @IBAction func touchButton(_ sender: UIButton) {
+        switch index {
+        case 0:
+            index += 1
+            redButton.alpha = 1
+        case 1:
+            index += 1
+            redButton.alpha = 0.3
+            yellowButton.alpha = 1
+        case 2:
+            index += 1
+            yellowButton.alpha = 0.3
+            greenButton.alpha = 1
+        default:
+            index = 0
+            redButton.alpha = 0.3
+            yellowButton.alpha = 0.3
+            greenButton.alpha = 0.3
+        }
+    }
+    
 }
 
